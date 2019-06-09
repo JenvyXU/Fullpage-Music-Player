a9555dbf88600d318dca6492146eef65813e6ed3
jenvyxu <1272117264@qq.com>
2019-06-09 11:26:41 +0800

    去除http协议

--- js/index.js
+++ js/index.js
@@ -70,7 +70,7 @@ var Footer = {
     },
     render: function() {
         var _this = this
-        $.getJSON('http://api.jirengu.com/fm/getChannels.php')
+        $.getJSON('//api.jirengu.com/fm/getChannels.php')
             .done(function(ret) {
                 _this.renderFooter(ret.channels)
             }).fail(function() {
